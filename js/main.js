fde6d9386323a4f2c71ee8010d23d30f8c6dcee7
Khondakar Shefat Hossain <shefathossain39@gmail.com>
2020-03-12 13:08:58 +0600

    set navigation

--- js/main.js
+++ js/main.js
@@ -11,7 +11,15 @@ $(document).ready(function(){
 
   /* Owl-crousel for blog */
 
-  $('.owl-carousel').owlCarousel();
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    autoplay:true,
+    autoplayTimeout:3000,
+    dots:false,
+    nav:true,
+    navText:[$('.owl-navigation .owl-nav-prev'),$('.owl-navigation .owl-nav-next')]
+
+  });
 
 
 
